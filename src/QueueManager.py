55cc9593130709aeb15936ef5da434422b3f648e
tvdhout <thijsvandenhout@live.nl>
2021-01-22 02:06:02 +0100

    Add role functionality, archive, help

--- src/QueueManager.py
+++ src/QueueManager.py
@@ -1,9 +1,8 @@
 from sys import stderr
-from typing import List
-
+from typing import List, Set
 import discord
 import asyncio
-from discord import Reaction, User, Member, Embed
+from discord import Reaction, User, Member, Embed, Message, Guild
 from discord.ext import commands
 import mysql.connector
 
@@ -11,49 +10,138 @@ from config import PREFIX, TOKEN
 
 
 class QueueManager(commands.Bot):
-    def __init__(self, dbconnection: mysql.connector.MySQLConnection, command_prefix: str):
-        super().__init__(command_prefix)
+    def __init__(self, dbconnection: mysql.connector.MySQLConnection, **kwargs):
+        super().__init__(**kwargs)
         self.dbconnection = dbconnection
-        self.question_channels = []
+
+    def get_queue_channels(self, guild: Guild) -> List[int]:
+        server_id = str(guild.id)
+        cursor = self.dbconnection.cursor()
+        cursor.execute("SELECT queues FROM servers WHERE serverid = %s",
+                       (server_id,))
+        try:
+            channels_string = cursor.fetchall()[0][0]
+            channels = channels_string.split()
+            channels = list(map(int, channels))  # cast to ints
+            return channels
+        except (IndexError, AttributeError):  # server not in database or question channels not set
+            return []
+
+    def get_manager_roles(self, guild: Guild) -> Set[int]:
+        server_id = str(guild.id)
+        cursor = self.dbconnection.cursor()
+        cursor.execute("SELECT roles FROM servers WHERE serverid = %s",
+                       (server_id,))
+        try:
+            roles_string = cursor.fetchall()[0][0]
+            roles = roles_string.split()
+            roles = set(map(int, roles))  # cast to ints
+            return roles
+        except (IndexError, AttributeError):  # server not in database or question channels not set
+            return set()
+
+    def is_manager(self, member: Member, manager_roles: Set[int] = None):
+        if manager_roles is None:
+            manager_roles = self.get_manager_roles(member.guild)
+        member_roles = set(map(lambda r: r.id, member.roles))
+        return len(member_roles & manager_roles) > 0  # Intersect is non-empty means they have a manager role.
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="to ?help"))
+
+    async def archive(self, message: Message):
+        """
+        Archive this message and those in the same chain
+        """
+        cursor = self.dbconnection.cursor()
+        cursor.execute("SELECT archiveid FROM servers WHERE serverid = %s",
+                       (str(message.guild.id),))
+        try:
+            archive_id = cursor.fetchall()[0][0]
+            self.dbconnection.commit()
+            channel = message.guild.get_channel(int(archive_id))
+        except (IndexError, TypeError):  # No archive channel set
+            m = await message.channel.send(f"There is not yet an archive channel for this server. Use the "
+                                           f"`{PREFIX}archive` command in the channel you wish to use as archive.")
+            await asyncio.sleep(8)
+            await m.delete()
+            return
+
+        if channel is None:  # Something went wrong if this is the case
+            print(f"Channel {archive_id} does not exist.", file=stderr)
+            return
+        embed = Embed(title=f"Question by {message.author.name}#{message.author.discriminator}",
+                      timestamp=message.created_at,
+                      thumbnail=message.author.avatar_url,
+                      colour=0xff0000)
+
+        history = await message.channel.history(limit=30).flatten()  # Look for chain to archive
+        manager_roles = self.get_manager_roles(message.author.guild)
+        chain = False
+        for m in history[::-1]:  # Reverse the list (old to new)
+            if m == message:
+                embed.add_field(name=f"{m.author.display_name}:", value=message.content, inline=False)
+                chain = True
+                await m.delete()
+                continue
+            if not chain:
+                continue
+            member = m.guild.get_member(m.author.id)
+            if m.author != message.author and not self.is_manager(member, manager_roles):
+                break
+            if self.is_manager(member, manager_roles):
+                continue
+            embed.add_field(name=f"{m.author.display_name}:", value=m.content, inline=False)
+            await m.delete()
+
+        await channel.send(embed=embed)
 
     async def on_message(self, message: discord.Message):
-        # TODO: just in ?questions channels
-        if message.author.id == self.user.id:
-            return  # The bot should not react to its own message
-        if len(message.author.roles) != 1:
+        if message.author.id == self.user.id:  # The bot should not react to its own message
+            return
+        # The bot should not be concerned with any channel that is not a queue, and should not react to manager roles.
+        if message.channel.id not in self.get_queue_channels(message.guild) or self.is_manager(message.author):
             await self.process_commands(message)
-            return  # The bot should not react to messages from roles other than @everyone
-        try:
-            previous_message = (await message.channel.history(limit=2).flatten())[1]
-            if previous_message.author == message.author:
-                return  # previous message was from the same user, don't react
-        except IndexError:
-            pass
+            return
+
+        chain = False  # This is not the continuation of a previous message until proven otherwise
+        manager_roles = self.get_manager_roles(message.author.guild)
+        async for prev_message in message.channel.history(limit=30):  # Look in message history for chain
+            if prev_message == message:  # Don't look at the current message
+                continue
+            if prev_message.author == message.author:  # This is a continuation of a previous message.
+                chain = True
+                break
+            member = message.guild.get_member(prev_message.author.id)
+            if self.is_manager(member, manager_roles):
+                continue
+            break
+        if chain:
+            return
+
         await message.add_reaction('📥')
         await self.process_commands(message)
 
-    async def on_reaction_add(self, reaction: Reaction, user: Member):
-        if isinstance(user, User) or user == self.user:
-            return  # Reaction is in DM, or the bot added the reaction
-        if len(user.roles) == 1:  # student added reaction
-            await reaction.remove(user)
+    async def on_reaction_add(self, reaction: Reaction, member: Member):
+        if isinstance(member, User) or member == self.user:  # Reaction is in a DM, or the bot added the reaction
             return
-        if reaction.emoji == '📥':
+        if not self.is_manager(member):  # Not a manager
+            await reaction.remove(member)
+            return
+        if reaction.emoji == '📥':  # TA clicked to claim this message.
             await reaction.message.clear_reactions()
             await reaction.message.add_reaction('📤')
             cursor = self.dbconnection.cursor()
             cursor.execute("INSERT IGNORE INTO messages "
                            "(messageid, ownerid) VALUES (%s, %s)",
-                           (str(reaction.message.id), str(user.id)))
+                           (str(reaction.message.id), str(member.id)))  # Set that TA as owner of this question.
             if cursor.rowcount > 0:
-                reply = await reaction.message.reply(f"{user.mention} will answer your question.")
+                reply = await reaction.message.reply(f"{member.mention} will answer your question.")
                 await asyncio.sleep(5)
                 await reply.delete()
             self.dbconnection.commit()
-        elif reaction.emoji == '📤':
+        elif reaction.emoji == '📤':  # TA clicked to archive this message.
             cursor = self.dbconnection.cursor()
             cursor.execute("SELECT ownerid FROM messages WHERE messageid = %s",
                            (str(reaction.message.id),))
@@ -64,27 +152,18 @@ class QueueManager(commands.Bot):
                 await reaction.message.clear_reactions()
                 await reaction.message.add_reaction('📥')
                 return
-            if owner_id != str(user.id):
-                await reaction.remove(user)
+            if owner_id != str(member.id):  # If the TA did not claim the message, they need to confirm their request.
+                await reaction.remove(member)
+                await reaction.message.add_reaction('✅')
+                await asyncio.sleep(4)
+                try:
+                    await reaction.message.remove_reaction('✅', self.user)  # Okay, never mind.
+                except discord.errors.NotFound:  # They confirmed.
+                    pass
                 return
-            cursor.execute("SELECT archiveid FROM servers WHERE serverid = %s",
-                           (str(user.guild.id),))
-            try:
-                archive_id = cursor.fetchall()[0][0]
-                self.dbconnection.commit()
-            except IndexError:
-                print("Index Error 69")
-                return
-            embed = Embed(title=f"Question by {reaction.message.author.name}",
-                          timestamp=reaction.message.created_at,
-                          thumbnail=reaction.message.author.avatar_url,
-                          colour=0xff0000)
-            embed.add_field(name=f"Content:",
-                            value=reaction.message.content)
-            channel = user.guild.get_channel(int(archive_id))
-            if channel is not None:
-                await channel.send(embed=embed)
-                await reaction.message.delete()
+            await self.archive(reaction.message)
+        elif reaction.emoji == '✅':
+            await self.archive(reaction.message)
 
 
 if __name__ == "__main__":
@@ -93,7 +172,10 @@ if __name__ == "__main__":
                                              host='localhost',
                                              database='queuemanager')
         try:
-            client = QueueManager(dbconnection=connection, command_prefix=PREFIX)
+            intents = discord.Intents.default()
+            intents.members = True  # Need the members intent to use guild.get_member in on_message
+            client = QueueManager(dbconnection=connection, command_prefix=PREFIX, intents=intents)
+            client.remove_command('help')
             client.load_extension('commands')
             client.run(TOKEN)
         except KeyboardInterrupt:
--- src/commands.py
+++ src/commands.py
@@ -1,7 +1,9 @@
+from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Context
 import re
 from QueueManager import QueueManager
+from config import PREFIX
 
 
 class CommandsCog(commands.Cog):
@@ -21,22 +23,74 @@ class CommandsCog(commands.Cog):
         self.client.dbconnection.commit()
         await context.send(f"{context.channel.mention} is now set as the archive channel.")
 
-    # @commands.command(name='questions')
-    # @commands.has_permissions(administrator=True)
-    # async def set_question_channels(self, context: Context):
-    #     channels = context.message.content.split()[1:]
-    #     if len(channels) == 0:
-    #         await context.send("Tag the channels to select as question channels as arguments to this command.")
-    #         return
-    #     channels = list(map(lambda c: re.sub('[><#]', '', c), channels))
-    #     channels_string = " ".join(channels)
-    #     server_id = str(context.guild.id)
-    #     cursor = self.client.dbconnection.cursor()
-    #     cursor.execute("INSERT INTO servers "
-    #                    "(serverid, questionschannels) VALUES (%s, %s) "
-    #                    "ON DUPLICATE KEY UPDATE  questionschannels = VALUES(questionschannels)",
-    #                    (server_id, channels_string))
-    #     self.client.dbconnection.commit()
+    @commands.command(name='queue')
+    @commands.has_permissions(administrator=True)
+    async def set_queue_channels(self, context: Context):
+        channels = context.message.content.split()[1:]
+        if len(channels) == 0:
+            await context.send(f"Tag the channels to enable as queue channel the in command's arguments: "
+                               f"`{PREFIX}questions #questions1 #questions2`.")
+            return
+        channels = list(map(lambda c: re.sub('[><#]', '', c), channels))
+        channels_string = " ".join(channels)
+        server_id = str(context.guild.id)
+        cursor = self.client.dbconnection.cursor()
+        cursor.execute("INSERT INTO servers "
+                       "(serverid, queues) VALUES (%s, %s) "
+                       "ON DUPLICATE KEY UPDATE  queues = VALUES(queues)",
+                       (server_id, channels_string))
+        self.client.dbconnection.commit()
+        self.client.get_queue_channels(context.guild)
+        await context.send(f"The channels used as queues are: {', '.join(context.message.content.split()[1:])}")
+
+    @commands.command(name='roles')
+    @commands.has_permissions(administrator=True)
+    async def set_manager_roles(self, context: Context):
+        roles = context.message.content.split()[1:]
+        if len(roles) == 0:
+            await context.send(f"Tag the roles to be allowed to manage queues in the command's arguments: "
+                               f"`{PREFIX}roles @Role1 @Role2`.")
+            return
+        roles = list(map(lambda r: re.sub('[><@&]', '', r), roles))
+        roles_string = " ".join(roles)
+        server_id = str(context.guild.id)
+        cursor = self.client.dbconnection.cursor()
+        cursor.execute("INSERT INTO servers "
+                       "(serverid, roles) VALUES (%s, %s) "
+                       "ON DUPLICATE KEY UPDATE  roles = VALUES(roles)",
+                       (server_id, roles_string))
+        self.client.dbconnection.commit()
+        self.client.get_queue_channels(context.guild)
+        await context.send(f"The roles that can manage queues are: {', '.join(context.message.content.split()[1:])}")
+
+    @commands.command(name='help')
+    @commands.has_permissions(administrator=True)
+    async def help_command(self, context: Context):
+        embed = Embed(title="Help", colour=0xffff00)
+        embed.add_field(name=f"Setup",
+                        value="This bot is used to manage a queue of questions and archive them "
+                              "when answered. This bot requires a small setup to be functional. You must set the "
+                              "channel in which to archive messages, the channels which are treated as queues, "
+                              "and the roles that can manage queues. See below on how to declare these.",
+                        inline=False)
+        embed.add_field(name="Command usage",
+                        value=f"`{PREFIX}help` → Show this menu.\n"
+                              f"`{PREFIX}archive` → Use this command in the channel you want to use as archive.\n"
+                              f"`{PREFIX}queue #channels` → Declare channels as queues. You can "
+                              f"tag one or multiple channels: `{PREFIX}queue #channel` / `{PREFIX}queue #channel1 "
+                              f"#channel2 ...`\n"
+                              f"`{PREFIX}roles` → Declare roles as queue managers. You can tag one or multiple roles:\n"
+                              f"`{PREFIX}roles @Role` / `{PREFIX}roles @Role1 @Role2 ...`")
+        embed.add_field(name="Queue management",
+                        value="When a regular user sends a message in a queue channel, the bot wil reply with "
+                              ":inbox_tray:. Consecutive messages by the same user (ignoring interruptions by "
+                              "managers) are regarded as one. A queue manager can click on the :inbox_tray: reaction "
+                              "to claim the question. Once answered it can be archived by clicking on the "
+                              ":outbox_tray:. Queue managers that are not the claimer of a question can still archive "
+                              "it, after clicking on the :white_check_mark: for confirmation, to avoid accidentally "
+                              "archiving a message you did not claim.",
+                        inline=False)
+        await context.send(embed=embed)
 
 
 def setup(client: QueueManager):
