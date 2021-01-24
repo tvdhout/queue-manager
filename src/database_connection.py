c4e9b41e6bf3840a6b16fb15c9e81fd09ba272e3
tvdhout <thijsvandenhout@live.nl>
2021-01-24 14:29:04 +0100

    Add docstring and make query data optional

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,13 +1,25 @@
 from sys import stderr
-from typing import Tuple
+from typing import Tuple, Optional, Union
 import mysql.connector
 
 
-def execute_query(query: str, data: Tuple, return_result: bool = False, return_cursor_count: bool = False):
+def execute_query(query: str, data: Optional[Tuple] = None, return_result: bool = False,
+                  return_cursor_count: bool = False) -> Union[None, list, int]:
+    """
+    Execute the given query on the database.
+    @param query: str: The query to execute
+    @param data: Optional[Tuple] the data to include in the query
+    @param return_result: bool: Whether or not to return the results from the query
+    @param return_cursor_count: Whether or not to return the number of rows affected in the database
+    @return: None, list or int
+    """
     try:  # Connect to database.
         connection = mysql.connector.connect(user='thijs', host='localhost', database='queuemanager')
         cursor = connection.cursor(buffered=True)
-        cursor.execute(query, data)
+        if data is not None:
+            cursor.execute(query, data)
+        else:
+            cursor.execute(query)
         result = None
         if return_result:
             result = cursor.fetchall()
