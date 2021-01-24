3b6cd4200ec03a7715c34bcaef83944d3ef89c32
tvdhout <thijsvandenhout@live.nl>
2021-01-24 14:27:59 +0100

    Change types, use the actual objects instead of their ID

--- src/server_conf.py
+++ src/server_conf.py
@@ -1,15 +1,19 @@
-from typing import List, Set, Optional, Tuple
-from discord import Guild
+from typing import Set, Optional, Tuple
+from discord import Guild, TextChannel, Role
 
 from database_connection import execute_query
 
 
 class ServerConfiguration:
     def __init__(self, server: Guild):
+        self.server = server
         self.server_id = server.id
-        self.archive_id, self.queue_ids, self.role_ids = self.request_server_conf()
+        self.archive: Optional[TextChannel] = None
+        self.queues: Set[TextChannel] = set()
+        self.roles: Set[Role] = set()
+        self.archive, self.queues, self.roles = self.request_server_conf()
 
-    def request_server_conf(self) -> Tuple[Optional[int], List[int], Set[int]]:
+    def request_server_conf(self) -> Tuple[Optional[TextChannel], Set[TextChannel], Set[Role]]:
         """
         Request the server configuration from the database
         @return: archive_id: Optional[int], queue_ids: List[int], role_ids: Set[int]
@@ -18,30 +22,35 @@ class ServerConfiguration:
                                (str(self.server_id),),
                                return_result=True)
         try:
-            archive_id, queues, roles = result[0]
+            archive_id, queue_ids, role_ids = result[0]
 
             if archive_id is not None:  # Archive channel is set
                 archive_id = int(archive_id)
-            if queues is not None:  # At least one queue channel is set
-                queues = queues.split()
-                queues = list(map(int, queues))
+                archive = self.server.get_channel(archive_id)
             else:
-                queues = []
-            if roles is not None:  # At least one role is designated queue manager
-                roles = roles.split()
-                roles = set(map(int, roles))
+                archive = None
+            if queue_ids is not None:  # At least one queue channel is set
+                queue_ids = queue_ids.split()
+                queue_ids = set(map(int, queue_ids))
+                queues = set(map(self.server.get_channel, queue_ids))
+            else:
+                queues = set()
+            if role_ids is not None:  # At least one role is designated queue manager
+                role_ids = role_ids.split()
+                role_ids = set(map(int, role_ids))
+                roles = set(map(self.server.get_role, role_ids))
             else:
                 roles = set()
 
-            return archive_id, queues, roles
+            return archive, queues, roles
         except IndexError:  # Server has no configuration (no entry in database)
-            return None, [], set()
+            return None, set(), set()
 
-    def set_archive_id(self, archive_id: int):
-        self.archive_id = archive_id
+    def set_archive(self, archive: Optional[TextChannel]) -> None:
+        self.archive = archive
 
-    def set_queue_ids(self, queue_ids: List[int]):
-        self.queue_ids = queue_ids
+    def set_queues(self, queues: Set[TextChannel]) -> None:
+        self.queues = queues
 
-    def set_role_ids(self, role_ids: Set[int]):
-        self.role_ids = role_ids
+    def set_roles(self, roles: Set[Role]) -> None:
+        self.roles = roles
